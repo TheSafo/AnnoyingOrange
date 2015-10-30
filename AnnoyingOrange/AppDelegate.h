98eb593d479863d3efeb3cbd0b9e0dccb48751fb
Jake Saferstein <jakesafo@gmail.com>
2015-10-30 03:54:12 -0400

    Initial Commit

---
+++ AnnoyingOrange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnnoyingOrange
+//
+//  Created by Jake Saferstein on 10/30/15.
+//  Copyright © 2015 Jake Saferstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnnoyingOrange/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AnnoyingOrange
+//
+//  Created by Jake Saferstein on 10/30/15.
+//  Copyright © 2015 Jake Saferstein. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
